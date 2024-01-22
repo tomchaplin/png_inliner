5e534e353f1c1eddd5b0f35c971fe690c3a4224a
Thomas Chaplin <tomrchaplin@gmail.com>
2024-01-22 15:27:49 +0000

    Initial commit

---
+++ justfile
@@ -0,0 +1,2 @@
+build:
+	.venv/bin/python -m nuitka --output-filename=png_inliner --onefile png_inliner.py
---
+++ png_inliner.py
@@ -0,0 +1,30 @@
+#!/home/tom/.scripts/png_inliner/.venv/bin/python
+
+import io
+import sys
+import base64
+from bs4 import BeautifulSoup
+
+file = sys.argv[1]
+
+with open(file, "r") as f:
+    raw_html = f.read()
+
+soup = BeautifulSoup(raw_html, "html.parser")
+
+for imgTag in soup.find_all("img"):
+    src = imgTag["src"]
+    is_external = src.startswith("http")
+    if is_external:
+        continue
+
+    if not src.endswith(".png"):
+        continue
+
+    with open(src, "rb") as img_file:
+        img_byte_str = base64.b64encode(img_file.read()).decode("utf-8")
+
+    new_src = "data:image/png;base64," + img_byte_str
+    imgTag["src"] = new_src
+
+print(soup)
